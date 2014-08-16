672446c50ca092f10c817215dc58b35e1ca98327
dalek10 <dalek10@10.0.1.144>
2014-08-16 14:51:25 -0700

    Initial commit

---
+++ yelpAPITest/src/yelpMain.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class yelpMain {
+	public static void main(String[] args) {
+		JFrame mainJFrame = new JFrame();
+		mainJFrame.setSize(300, 750);
+		mainJFrame.setVisible(true);
+		mainJFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	}
+}
